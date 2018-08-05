e120457cabd4672616ddbc0b0a733e28225b7047
jinzzam <firstmind_@naver.com>
2018-08-06 00:32:59 +0900

    get json

--- android/app/src/main/java/com/example/jinzzam/myapplication/MainActivity.java
+++ android/app/src/main/java/com/example/jinzzam/myapplication/MainActivity.java
@@ -2,12 +2,61 @@ package com.example.jinzzam.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MAIN";
+    private TextView tv;
+    private RequestQueue queue;
+
+    private String name;
+    private String id;
+    private String pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+
+        queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.43.36:3000/users";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JsonParser jsonParser = new JsonParser();
+                JsonElement jsonElement = jsonParser.parse(response);
+                tv.setText(jsonElement + "\n");
+                tv.setText(tv.getText() + jsonElement.getClass().getName() + "\n");
+                tv.setText(tv.getText() + ((JsonObject) jsonElement).get("name").getClass().getName() + "\n");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        stringRequest.setTag(TAG);
+        queue.add(stringRequest);
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (queue != null) {
+            queue.cancelAll(TAG);
+        }
     }
 }
--- webserver/routes/users.js
+++ webserver/routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  var myJson = {'name' : 'youjin'}
-  res.send(myJson);
+router.get('/', function (req, res, next) {
+    var myJson = {'name': 'youjin', 'id': 'jinzzam', 'pw': '1234'}
+    res.send(myJson);
 });
 
 module.exports = router;
