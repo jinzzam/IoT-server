0c28d42f93642631eabb22ea0bde4e49cfab1673
jinzzam <firstmind_@naver.com>
2018-08-05 23:59:51 +0900

    json web server test

--- webserver/routes/users.js
+++ webserver/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var myJson = {'name' : 'youjin'}
+  res.send(myJson);
 });
 
 module.exports = router;
